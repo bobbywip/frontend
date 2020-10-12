1633c09b4b98429ca651811b8cd53a6122b81325
HARRY DENLEY <HARRY.DENLEY@OUTLOOK.COM>
2020-10-12 23:31:49 +0100

    Removed import of unneeded component

--- src/components/dashboard/header/index.js
+++ src/components/dashboard/header/index.js
@@ -1,8 +1,6 @@
 import React from "react"
 import styled from "styled-components"
 
-import { PrimaryButton } from "../../common/buttons"
-
 const Container = styled.div`
     width: 100%;
     display: grid;
