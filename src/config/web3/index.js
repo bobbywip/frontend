12583899caa421739896e27d1ee25cf02a1ccad5
HARRY DENLEY <HARRY.DENLEY@OUTLOOK.COM>
2021-01-23 17:41:19 +0000

    Added logic to add icons to show if the campaigns on the current epoch have been voted on

--- src/config/web3/index.js
+++ src/config/web3/index.js
@@ -1,6 +1,7 @@
 const KNC_TOKEN_ABI = require('./contracts/abis/knc_token_abi.json')
 const KNC_STAKING_ABI = require('./contracts/abis/knc_staking_abi.json')
 const KCSP_CONTRACT_ABI = require('./contracts/abis/kscp_abi.json')
+const KNC_DAO_ABI = require('./contracts/abis/knc_dao_abi.json')
 const CONTRACT_CONFIG = require('./contracts/config.json')
 
 const WEB3SETTINGS = {
@@ -15,4 +16,4 @@ const WEB3SETTINGS = {
     }
 }
 
-export { WEB3SETTINGS, KNC_TOKEN_ABI, KNC_STAKING_ABI, KCSP_CONTRACT_ABI }
+export { WEB3SETTINGS, KNC_TOKEN_ABI, KNC_STAKING_ABI, KCSP_CONTRACT_ABI, KNC_DAO_ABI }
