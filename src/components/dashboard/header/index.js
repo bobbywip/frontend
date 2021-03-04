b7673e0d9e49bf571e5d1d47cfda9f7af7aed350
HARRY DENLEY <HARRY.DENLEY@OUTLOOK.COM>
2021-03-04 20:11:45 +0000

    Added logic to show a link to the guide because of node issues

--- src/components/dashboard/header/index.js
+++ src/components/dashboard/header/index.js
@@ -18,10 +18,22 @@ const Title = styled.h2`
     letter-spacing: 0.1px;
     color: #6229FF;
 `
+const InfoBar = styled.div`
+    width: 100%;
+    display: block;
+    background: #ffedaa;
+    border: 2px solid #dbcc93;
+    padding: 0.5em;
+    border-radius: 0.5em;
+`
 
 export default function Header() {
   return (
     <Container>
+        <InfoBar>
+          ⚠️ We are currently facing node issues meaning some values will not be reflected correctly on this UI.{' '}
+          Please see the "Troubleshooting" section in <a href="https://mandelliant.medium.com/how-to-stake-knc-and-earn-rewards-with-the-kyber-community-pool-fa9142f2ca8a" target="_blank" rel="noreferrer noopener">this guide</a>.
+        </InfoBar>
         <Title>Kyber Community Staking Dashboard</Title>
         <DelegatedAmount />
     </Container>
